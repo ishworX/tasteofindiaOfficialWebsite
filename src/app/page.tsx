b2be270a2e34a25ed1948e03d2dddd5673d4c463
ishworX <git.ishwor@gmail.com>
2024-09-12 23:44:51 -0600

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "./sections/Hero/Hero";
+import About from "./sections/About/About";
+import Menu from "./sections/Menu/Menu";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <main id="main">
+        {/* <About />
+        <Menu /> */}
+      </main>
     </>
     
   );
