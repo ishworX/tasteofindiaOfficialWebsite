25eb391c14bf1e646518536783ce1a2b81adfc71
ishworX <git.ishwor@gmail.com>
2024-09-18 23:56:11 -0600

    testing?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,10 +39,6 @@ export default function RootLayout({
         <Header />
         {children}
         </body>
-      {/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh950GNyZPhc TNXj1NW7RuBCsyN/00jlpcV8Qyq46cDfL"
-        crossOrigin="anonymous">
-      </script> */}
     </html>
   );
 }
